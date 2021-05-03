af500d2d0c2212dcd5fcebed90be324a965490e5
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-03 22:59:00 +0200

    Added attribute [numbers] to class Ticket

--- lotto_game/ticket.py
+++ lotto_game/ticket.py
@@ -1,9 +1,11 @@
 class Ticket:
 
-    def __init__(self, num_ticket, bet, city):
+    def __init__(self, num_ticket, bet, city, numbers):
         self.num_ticket = num_ticket
         self.bet = bet
         self.city = city
+        self.numbers = numbers
+
 
     def __str__(self):
         return (f"""
