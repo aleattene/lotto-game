66fed7bb3be54cc4e70b5648d3de884e7c5dc7f2
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-03 22:20:00 +0200

    Added function generate_numbers

--- lotto_game/helper.py
+++ lotto_game/helper.py
@@ -1,3 +1,18 @@
+from random import randint
+
+
+# This function returns an ordered list of integers (between MIN_NUMBER and MAX_NUMBERS)
+def generate_numbers(n):
+    MIN_NUMBER = 1
+    MAX_NUMBER = 90
+    numbers = []
+    while n > 0:
+        number = randint(MIN_NUMBER, MAX_NUMBER + 1)
+        if number not in numbers:
+            numbers.append(number)
+            n -= 1
+    numbers.sort()
+    return numbers
 
 
 
