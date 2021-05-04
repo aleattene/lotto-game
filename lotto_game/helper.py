b06b64bec7b9da645aae8f7c3d46fb3e7044b917
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-04 17:40:22 +0200

    Removed function print_tickets (it has become a static method of the class Output)

--- lotto_game/helper.py
+++ lotto_game/helper.py
@@ -1,6 +1,5 @@
 from random import randint
 
-
 # This function returns an ordered list of integers (between MIN_NUMBER and MAX_NUMBERS)
 def generate_numbers(n):
     MIN_NUMBER = 1
@@ -15,12 +14,6 @@ def generate_numbers(n):
     return numbers
 
 
-# This function prints all of the played tickets
-def print_tickets(tickets):
-    for k in sorted(tickets):
-        print("Ticket n. {}".format(k))
-        print("Numbers : {}".format(tickets[k][0]))
-        print("Wheel: {}".format(tickets[k][1]))
-        print("Bet Type : {}".format(tickets[k][2]))
+
 
 
