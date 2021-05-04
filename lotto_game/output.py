93026f16e4f9cf42fe8d1bd0616e3d60c0ea9c89
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-04 17:41:04 +0200

    Added static method "print_tickets"

--- lotto_game/output.py
+++ lotto_game/output.py
@@ -1,2 +1,14 @@
 class Output:
-    pass
+
+    # This method receives the dictionary of tickets played and prints them
+    @staticmethod
+    def print_tickets(tickets_played):
+        for k in sorted(tickets_played):
+            # ... complete responsive layout
+            print("╔" + "═" * 20 + "╗")     # ASCII code (201,205,187)
+            print("║     Ticket n. {}    ║".format(k))     # ASCII code (186,186)
+            print("╠" + "═" * 20 + "╣")     # ASCII code (204,205,185)
+            print("║ Numbers : {}     ║".format(tickets_played[k][0]))
+            print("║ Wheel: {}        ║".format(tickets_played[k][1]))
+            print("║ Bet Type : {}  ║".format(tickets_played[k][2]))
+            print("╚" + "═" * 20 + "╝")     # ASCII code (200,205,188)
