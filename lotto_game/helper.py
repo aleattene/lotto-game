5e5c1e08a3e4efa4463a1eb0e5dd33ceaa87fdb2
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-05 15:31:59 +0200

    Added static method "storage_ticket"

--- lotto_game/helper.py
+++ lotto_game/helper.py
@@ -3,19 +3,26 @@ from random import randint
 
 class Helper:
 
-    # This method returns an ordered list of integers (between MIN_NUMBER and MAX_NUMBERS)
+    # This method returns an ordered list of integers (between min_number and max_number)
     @staticmethod
-    def generate_numbers(num_tickets):
-        MIN_NUMBER = 1
-        MAX_NUMBER = 90
+    def generate_numbers(num_tickets, min_number, max_number):
         numbers = []
         while num_tickets > 0:
-            number = randint(MIN_NUMBER, MAX_NUMBER + 1)
+            number = randint(min_number, max_number + 1)
             if number not in numbers:
                 numbers.append(number)
                 num_tickets -= 1
         numbers.sort()
         return numbers
+
+    # Storage of the played tickets
+    @staticmethod
+    def storage_ticket(tickets_played, ticket):
+        tickets_played[ticket.num_ticket] = (ticket.numbers, ticket.city, ticket.bet)
+
+
+
+
 
 
 
