887f24d9344a08879547c6b8c456cb60d1fa5518
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-05 11:44:27 +0200

    Renamed parameter "n" to "num_tickets"

--- lotto_game/helper.py
+++ lotto_game/helper.py
@@ -5,15 +5,15 @@ class Helper:
 
     # This method returns an ordered list of integers (between MIN_NUMBER and MAX_NUMBERS)
     @staticmethod
-    def generate_numbers(n):
+    def generate_numbers(num_tickets):
         MIN_NUMBER = 1
         MAX_NUMBER = 90
         numbers = []
-        while n > 0:
+        while num_tickets > 0:
             number = randint(MIN_NUMBER, MAX_NUMBER + 1)
             if number not in numbers:
                 numbers.append(number)
-                n -= 1
+                num_tickets -= 1
         numbers.sort()
         return numbers
 
