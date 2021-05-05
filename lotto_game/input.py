8c4c60b6fab6b4f4e3a051a21ea3e00ba67e7ac0
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-05 10:33:29 +0200

    Added static method "acquire_bet_type"

--- lotto_game/input.py
+++ lotto_game/input.py
@@ -19,3 +19,22 @@ class Input:
             except ValueError:
                 print("Incorrect Entry. Try Again.")
         return num_tickets
+
+    # Acquisition of the type of bet
+    @staticmethod
+    def acquire_bet_type():
+        # Dictionary containing the type of bet
+        all_bets = {1: "Ambata", 2: "Ambo", 3: "Terno", 4: "Quaterna", 5: "Cinquina"}
+        for k in all_bets:
+            print("  {} : {}".format(k, all_bets[k]))
+        while True:
+            try:
+                bet_key = int(input("Bet Type ? : "))  # type int
+                if bet_key in all_bets:
+                    break
+                else:
+                    raise ValueError
+            except ValueError:
+                print("Incorrect Entry. Try Again")
+        bet_name = all_bets[bet_key]
+        return bet_name
