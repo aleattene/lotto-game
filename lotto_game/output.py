1d6b039093ab78a4e6922cddcdc05366dad5f0ae
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-05 15:30:24 +0200

    Updated function print_ticket (updated layout of print)

--- lotto_game/output.py
+++ lotto_game/output.py
@@ -5,10 +5,13 @@ class Output:
     def print_tickets(tickets_played):
         for k in sorted(tickets_played):
             # ... complete responsive layout
-            print("╔" + "═" * 20 + "╗")     # ASCII code (201,205,187)
-            print("║     Ticket n. {}    ║".format(k))     # ASCII code (186,186)
-            print("╠" + "═" * 20 + "╣")     # ASCII code (204,205,185)
-            print("║ Numbers : {}     ║".format(tickets_played[k][0]))
-            print("║ Wheel: {}        ║".format(tickets_played[k][1]))
-            print("║ Bet Type : {}  ║".format(tickets_played[k][2]))
-            print("╚" + "═" * 20 + "╝")     # ASCII code (200,205,188)
+            print("╔{:^43}╗".format("═"*43))     # ASCII code (201,205,187)
+            print("║{:^43}║".format("Ticket n. {}".format(k)))     # ASCII code (186,186)
+            print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,205,185)
+            print("║  Wheel:  {:^33}║".format(" {}".format(tickets_played[k][1])))  # ASCII code (186,186)
+            print("║  BetType:{:^33}║".format(" {}".format(tickets_played[k][2])))  # ASCII code (186,186)
+            numbers = ""
+            for value in tickets_played[k][0]:
+                numbers += str(value) + " "
+            print("║  Numbers:{:^33}║".format("{}".format(numbers)))  # ASCII code (186,186)
+            print("╚{:^43}╝".format("═" * 43))  # ASCII code (200,205,188)
