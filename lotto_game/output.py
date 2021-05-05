cd41c18620d74730e4090268bb02c362b631d46c
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-05 17:57:02 +0200

    Updated layout displayed by the function "print_tickets"

--- lotto_game/output.py
+++ lotto_game/output.py
@@ -3,8 +3,8 @@ class Output:
     # This method receives the dictionary of tickets played and prints them
     @staticmethod
     def print_tickets(tickets_played):
+        print()
         for k in sorted(tickets_played):
-            # ... complete responsive layout
             print("╔{:^43}╗".format("═" * 43))     # ASCII code (201,205,187)
             print("║{:^43}║".format("Ticket n. {}".format(k)))     # ASCII code (186,186)
             print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,205,185)
