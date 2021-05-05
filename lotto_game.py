d1d4536100742e20c5f75cdc4ee5c0801db16e73
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-05 11:19:03 +0200

    Refactoring section "acquisition amount of numbers to play" (encapsulation)

--- lotto_game.py
+++ lotto_game.py
@@ -8,8 +8,13 @@ from lotto_game.ticket import Ticket
 
 def main():
     # DECLARATION OF VARIABLES, CONSTANTS AND DATA STRUCTURES
+    # Minimum and maximum number of tickets that can be played
+    MIN_TICKETS = 1
+    MAX_TICKETS = 5
     # Maximum amount of numbers that can be played for each ticket
     MAX_NUMBERS = 10
+    # Dictionary containing the type of bet
+    all_bets = {1: "Ambata", 2: "Ambo", 3: "Terno", 4: "Quaterna", 5: "Cinquina"}
     # Dictionary containing the cities(aka "wheel") that can be chosen for the bet
     all_cities = {1: "Bari", 2: "Cagliari", 3: "Firenze", 4: "Genova", 5: "Milano",
                   6: "Napoli", 7: "Palermo", 8: "Roma", 9: "Torino", 10: "Venezia", 11: "Tutte"}
@@ -18,21 +23,17 @@ def main():
     tickets_played = {}
 
     # Acquisition of the number of tickets
-    num_tickets = Input.acquire_number_tickets()
+    num_tickets = Input.acquire_number_tickets(MIN_TICKETS, MAX_TICKETS)
 
-   # Tickets generation
+   # TICKETS GENERATION
     for i in range(1, num_tickets + 1):
-        print("##### TICKET N. {} #####".format(i))
-        # Entering the number of numbers to play
-        while True:
-            try:
-                numbers = int(input("Numbers (from {} to 10) : ".format(bet_key)))  # type int
-                if bet_key <= numbers <= MAX_NUMBERS:
-                    break
-                else:
-                    raise ValueError
-            except ValueError:
-                print("Incorrect Entry. Try Again")
+
+        # Acquisition of the type of bet
+        bet_key_name = Input.acquire_bet_type(i, all_bets)  # type tuple
+
+        # Acquisition of the amount of numbers to play
+        numbers = Input.acquire_amount_numbers(bet_key_name[0], MAX_NUMBERS)
+
         # Entering the wheel (city) to play
         for k in all_cities:
             print("  {}: {}".format(k, all_cities[k]))
@@ -48,8 +49,8 @@ def main():
         # Single Ticket Generation
         city_name = all_cities[city_key]
         city = City(city_name)  # new object of type City
-        bet_name = Input.acquire_bet_type()
-        bet = Bet(bet_name)     # new object of type Bet
+
+        bet = Bet(bet_key_name[1])     # new object of type BetType
         selected_numbers = Helper.generate_numbers(numbers)
         ticket = Ticket(i, bet.name, city.name, selected_numbers)
 
