84716a4aa6759f41c6c70df6afd5e714febe0857
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-07 13:28:54 +0200

    Fixed some comments and labels

--- lotto_game/bet_type.py
+++ lotto_game/bet_type.py
@@ -1,6 +1,6 @@
 class BetType:
 
-    # Dictionary containing the type of bet
+    # Dictionary containing all the type of lotto bet
     all_bet_types = {1: "Ambata", 2: "Ambo", 3: "Terno", 4: "Quaterna", 5: "Cinquina"}
 
     def __init__(self, id_bet_type, name):
@@ -10,19 +10,18 @@ class BetType:
     # This static method acquires from the user the type of bet
     @staticmethod
     def acquire_bet_type(num_ticket):
-
-        print("\n##### TICKET N. {} #####\n".format(num_ticket))
-        for k in BetType.all_bet_types:
+        print()
+        for k in BetType.all_bet_types:  # type INT
             print("       {}: {}".format(k, BetType.all_bet_types[k]))
         while True:
             try:
-                bet_key = int(input("\nEnter the Type of Bet "
-                                    "(between 1 and {}): ".format(len(BetType.all_bet_types))))  # type int
+                bet_key = int(input("\nEnter the type of bet for the ticket n. "    # type INT
+                                    "{} (between 1 and {}): ".format(num_ticket, len(BetType.all_bet_types))))
                 if bet_key in BetType.all_bet_types:
                     break
                 else:
                     raise ValueError
             except ValueError:
-                print("Incorrect Entry. Try Again")
-        bet_name = BetType.all_bet_types[bet_key]
-        return bet_key, bet_name
+                print("Incorrect Entry. Try Again.")
+        bet_name = BetType.all_bet_types[bet_key]   # type STR
+        return bet_key, bet_name  # type TUPLE (int,str)
