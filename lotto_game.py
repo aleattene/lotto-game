3ebf7a8e5c1d83f4cbf0f5befdaa2ee219c78521
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-07 13:29:29 +0200

    Fixed some comments and labels

--- lotto_game.py
+++ lotto_game.py
@@ -5,37 +5,36 @@ from lotto_game.ticket import Ticket
 
 
 def main():
-    # DATA STRUCTURE DECLARATION
 
-    # Acquisition of the number of tickets
+    # Acquisition of the number of tickets to generate
     range_tickets = Ticket.acquire_number_tickets()  # type TUPLE (num_min_tickets,num_max_tickets)
 
     # TICKETS GENERATION
     for i in range(range_tickets[0], range_tickets[1] + 1):
 
-        # Acquisition of the type of bet
-        bet_key_name = BetType.acquire_bet_type(i)  # type TUPLE
+        # Acquisition of the type of bet for each ticket
+        bet_key_name = BetType.acquire_bet_type(i)  # type TUPLE (int,str)
 
-        # Acquisition of the amount of numbers to play
+        # Acquisition of the amount of numbers to generate for each ticket
         amount_numbers = NumbersPlay.acquire_amount_numbers(i, bet_key_name[0])  # type INT
 
-        # Acquisition of the wheel(s) to play
-        city_key_name = City.acquire_city(i)  # type TUPLE
+        # Acquisition of the wheel/city to play for each ticket
+        city_key_name = City.acquire_city(i)  # type TUPLE (int,str)
 
         # SINGLE TICKET GENERATION
         # New Object of type City
         city = City(city_key_name[0], city_key_name[1])
         # New Object of type BetType
-        bet = BetType(bet_key_name[0], bet_key_name[1])
-        # Numbers generation
-        generated_numbers = NumbersPlay.generate_numbers(amount_numbers)  # type LIST
+        bet_type = BetType(bet_key_name[0], bet_key_name[1])
+        # Generation of the numbers to play for each ticket
+        generated_numbers = NumbersPlay.generate_numbers(amount_numbers)  # type LIST [int]
         # New Object of type Ticket
-        ticket = Ticket(i, bet.name, city.name, generated_numbers)
+        ticket = Ticket(i, bet_type.name, city.name, generated_numbers)
 
-        # STORAGE OF THE PLAYED TICKET
+        # STORAGE OF EACH TICKET PLAYED
         ticket.storage_ticket()
 
-    # PRINTING OF THE TICKETS PLAYED
+    # PRINT OF ALL TICKETS PLAYED
     Ticket.print_tickets()
 
 
