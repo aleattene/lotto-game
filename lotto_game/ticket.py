a2df047654854b8916af2114faa97cc8cae56a66
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-07 12:26:45 +0200

    Added static method "acquire_number_tickets"

--- lotto_game/ticket.py
+++ lotto_game/ticket.py
@@ -21,6 +21,27 @@ class Ticket:
     def storage_ticket(self):
         Ticket.tickets_played.append(self)
 
+    # This static method acquires from the user the number of tickets to generate
+    @staticmethod
+    def acquire_number_tickets():
+        # Minimum and maximum number of tickets that can be played
+        MIN_TICKETS = 1
+        MAX_TICKETS = 5
+        while True:
+            try:
+                num_tickets = int(input("\nHow many tickets do you want to generate (between "
+                                        "{} and {}, 0 to quit)? ".format(MIN_TICKETS, MAX_TICKETS)))  # type int
+                if num_tickets == 0:
+                    print("\nThank you for your attention.\nThe simulation of the lotto game is terminated.")
+                    quit()
+                elif MIN_TICKETS <= num_tickets <= MAX_TICKETS:
+                    break
+                else:
+                    raise ValueError
+            except ValueError:
+                print("Incorrect Entry. Try Again.")
+        return MIN_TICKETS, num_tickets
+
     # This static method prints the tickets played
     @staticmethod
     def print_tickets():
