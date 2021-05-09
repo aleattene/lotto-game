9f44c9a6d79eaf03835c01a16575dcdd6c23177b
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-09 10:44:26 +0200

    Added and modified some comments

--- lotto_game/extraction.py
+++ lotto_game/extraction.py
@@ -5,27 +5,32 @@ from datetime import datetime
 
 class Extraction:
 
+    # Dictionary that will contain the extraction {key -> city : value -> [numbers]}
     extraction = {}
 
+    # This static method perform the extraction (generates randomly 5 numbers for each wheel/city)
     @staticmethod
     def perform_extraction():
+        # Max amount of numbers that can be extracted for each wheel/city
         AMOUNT_NUMBERS_EXTRACTION = 5
         for k in sorted(City.all_cities):
-            extracted_numbers = Numbers.generate_numbers(AMOUNT_NUMBERS_EXTRACTION)
+            extracted_numbers = Numbers.generate_numbers(AMOUNT_NUMBERS_EXTRACTION)  # type LIST
+            # Dictionary Update
             Extraction.extraction[City.all_cities[k]] = extracted_numbers
 
+    # This static method print tha table of the extraction
     @staticmethod
     def print_extraction():
-        # TABLE HEADER
+        # Table header
         print("\n╔{:^43}╗".format("═" * 43))  # ASCII code (201,205,187)
         print("║{:^43}║".format("ESTRAZIONE del "
                                 "{}".format(datetime.today().strftime('%d/%m/%Y'))))  # ASCII code (186,186)
         print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,205,185)
-        # TABLE BODY
+        # Table body
         for city in sorted(Extraction.extraction):
             print("║   {:13}".format(city), end="")  # ASCII code (186)
             for value in Extraction.extraction[city]:
                 print("{:4}".format(value), end=" ")
             print("  ║")  # ASCII code (186)
-        # TABLE FOOTER
+        # Table FOOTER
         print("╚{:^43}╝".format("═" * 43))  # ASCII code (200,205,188)
