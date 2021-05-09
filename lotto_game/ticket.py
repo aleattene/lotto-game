0f7da716509945a7e3f758a4462661994a4d8f56
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-09 16:59:14 +0200

    Updated __init__ and modified method check_ticket"

--- lotto_game/ticket.py
+++ lotto_game/ticket.py
@@ -13,6 +13,9 @@ class Ticket:
         self.bet_type_name = bet_type_name
         self.city = city                # type OBJECT (class City)
         self.numbers = numbers          # type LIST
+        # .... result must be a list of OBJECTS (class Win) but doesn't work
+        self.result_city = []
+        self.result_numbers = []
 
     def __str__(self):
         return (f"""
@@ -26,23 +29,54 @@ class Ticket:
     def storage_ticket(self):
         Ticket.tickets_played.append(self)
 
+    # This instance method checks whether the ticket is winning or not
     def check_ticket(self):
-        if self.city.name != "Tutte":
-            count = 0
+        # Tuple that will contain the cities and extracted numbers for each win
+        wins = ()
+        if self.city.name != "Tutte":  # city choice: only one
+            # List that will contain the extracted numbers for each city
+            extracted_numbers = []
             for number in Extraction.extraction[self.city.name]:
                 if number in self.numbers:
-                    count += 1
-                    if count >= self.bet_type_id:
-                        return True, self.city.name
-        else:
+                    extracted_numbers.append(number)
+            if len(extracted_numbers) >= self.bet_type_id:
+                wins += self.city.name, extracted_numbers
+        else:   # cities chosen = "Tutte"
             for city in Extraction.extraction:
-                count = 0
+                extracted_numbers = []
                 for number in Extraction.extraction[city]:
                     if number in self.numbers:
-                        count += 1
-                        if count >= self.bet_type_id:
-                            return True, city
-        return False, ""
+                        extracted_numbers.append(number)
+                if len(extracted_numbers) >= self.bet_type_id:
+                    wins += city, extracted_numbers
+        return wins  # type TUPLE
+
+    # This method prints all the tickets played
+    def print_ticket(self):
+        print()
+        print("╔{:^43}╗".format("═" * 43))     # ASCII code (201,205,187)
+        print("║{:^43}║".format("Ticket n. {}".format(self.id_ticket)))     # ASCII code (186)
+        print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,205,185)
+        print("║    Wheel:  {:31}║".format(self.city.name))  # ASCII code (186)
+        # .... bet_type must be an OBJECT (class BetType) but doesn't work
+        print("║  BetType:  {:31}║".format(self.bet_type_name))  # ASCII code (186)
+        numbers = ""
+        for number in self.numbers:
+            numbers += str(number) + " "
+        print("║  Numbers:  {:31}║".format(numbers))  # ASCII code (186)
+        print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,205,185)
+        if self.result_city:  # non-empty list
+            print("║{:^43}║".format("Congratulations, you won."))
+            for i, city in enumerate(self.result_city):
+                print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,205,185)
+                print("║              Wheel: {:22}║".format(city))  # ASCII code (186)
+                numbers = ""
+                for value in self.result_numbers[i]:
+                    numbers += str(value) + " "
+                print("║  Extracted numbers: {:22}║".format(numbers))  # ASCII code (186)
+        else:  # False
+            print("║{:^43}║".format("{}".format("I’m sorry, you lost.")))  # ASCII code (186)
+        print("╚{:^43}╝".format("═" * 43))  # ASCII code (200,205,188)
 
     # This static method acquires from the user the number of tickets to generate
     @staticmethod
@@ -65,26 +99,4 @@ class Ticket:
                 print("Incorrect Entry. Try Again.")
         return MIN_TICKETS, num_tickets  # type TUPLE (int,int)
 
-    # This static method prints all the tickets played
-    @staticmethod
-    def print_tickets():
-        print()
-        for ticket in Ticket.tickets_played:
-            print("╔{:^43}╗".format("═" * 43))     # ASCII code (201,205,187)
-            print("║{:^43}║".format("Ticket n. {}".format(ticket.id_ticket)))     # ASCII code (186,186)
-            print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,205,185)
-            print("║    Wheel:  {:31}║".format(ticket.city.name))  # ASCII code (186,186)
-            # .... bet_type must be an OBJECT (class BetType) but doesn't work
-            print("║  BetType:  {:31}║".format(ticket.bet_type_name))  # ASCII code (186,186)
-            numbers = ""
-            for number in ticket.numbers:
-                numbers += str(number) + " "
-            print("║  Numbers:  {:31}║".format("{}".format(numbers)))  # ASCII code (186,186)
-            print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,205,185)
-            result = ticket.check_ticket()
-            if result[0]:  # True
-                print("║{:^43}║".format("{}".format("Congratulations, you won. Wheel: "
-                                                    "" + result[1])))  # ASCII code (186,186)
-            else:  # False
-                print("║{:^43}║".format("{}".format("I’m sorry, you didn't win.")))  # ASCII code (186,186)
-            print("╚{:^43}╝".format("═" * 43))  # ASCII code (200,205,188)
+
