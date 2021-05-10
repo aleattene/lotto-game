0b9fe4165299b55be5aa9ec8d0e0bf03d2d5bd29
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-10 16:04:41 +0200

    Modified some comments

--- lotto_game/extraction.py
+++ lotto_game/extraction.py
@@ -13,25 +13,25 @@ class Extraction:
     def perform_extraction():
         # Max amount of numbers that can be extracted for each wheel/city
         AMOUNT_NUMBERS_EXTRACTION = 5
-        for k in sorted(City.all_cities):
+        for k in sorted(City.all_cities):   # type(k) = INT
             if City.all_cities[k] != "Tutte":
                 extracted_numbers = NumberUtils.generate_numbers(AMOUNT_NUMBERS_EXTRACTION)  # type LIST
-                # Dictionary Update
+                # Dictionary Updating
                 Extraction.extraction[City.all_cities[k]] = extracted_numbers
 
     # This static method prints the table of the extraction
     @staticmethod
     def print_extraction():
-        # Table header
-        print("\n╔{:^43}╗".format("═" * 43))  # ASCII code (201,205,187)
-        print("║{:^43}║".format("ESTRAZIONE del "
-                                "{}".format(datetime.today().strftime('%d/%m/%Y'))))  # ASCII code (186)
-        print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,205,185)
-        # Table body
+        # Table HEADER
+        print("\n╔{:^43}╗".format("═" * 43))  # ASCII code (201,187,205)
+        print("║{:^43}║".format("ESTRAZIONE del "  # ASCII code (186)
+                                "{}".format(datetime.today().strftime('%d/%m/%Y'))))  # date format: DD/MM/YYYY
+        print("╠{:^43}╣".format("═" * 43))  # ASCII code (204,185,205)
+        # Table BODY
         for city in sorted(Extraction.extraction):
             print("║   {:13}".format(city), end="")  # ASCII code (186)
             for value in Extraction.extraction[city]:
                 print("{:4}".format(value), end=" ")
             print("  ║")  # ASCII code (186)
-        # Table footer
-        print("╚{:^43}╝".format("═" * 43))  # ASCII code (200,205,188)
+        # Table FOOTER
+        print("╚{:^43}╝".format("═" * 43))  # ASCII code (200,188,205)
