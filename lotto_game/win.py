d9ed233523dc363f9fc88d9af029c281df446531
Alessandro Attene <attene.tomorrowdevs@gmail.com>
2021-05-10 16:39:04 +0200

    Added some comments and modified some names of attribute/parameter

--- lotto_game/win.py
+++ lotto_game/win.py
@@ -3,7 +3,9 @@ from lotto_game.city import City
 
 class Win:
 
+    # Dictionary containing which and how many combinations can be achieved on a single wheel/city.
     combinations = {
+        # Numbers: [Ambata, Ambo, Terno, Quaterna, Cinquina]
         1: [1],
         2: [2, 1],
         3: [3, 3, 1],
@@ -11,7 +13,9 @@ class Win:
         5: [5, 10, 10, 5, 1]
     }
 
+    # Dictionary containing the gross amount of winnings obtained by betting € 1 on a single wheel/city.
     gross_winnings = {
+        # Numbers Played: [Ambata, Ambo, Terno, Quaterna, Cinquina]
         1: [11.23],
         2: [5.61, 250],
         3: [3.74, 83.33, 4500],
@@ -24,27 +28,36 @@ class Win:
         10: [1.12, 5.55, 37.50, 571.42, 23809.52]
     }
 
-    def __init__(self, city, extracted_numbers):
-        self.city = city
-        self.extracted_numbers = extracted_numbers
-        self.gross_amount = 0
-        self.net_amount = 0
+    def __init__(self, city_name, extracted_numbers):
+        self.city_name = city_name                  # type STR
+        self.extracted_numbers = extracted_numbers  # type LIST of INT
+        self.gross_amount = 0.0                     # type FLOAT
+        self.net_amount = 0.0                       # type FLOAT
 
     def __str__(self):
         return (f"""
-        City: {self.city}
+        City: {self.city_name}
         Numbers: {self.extracted_numbers}
+        Gross Amount: {self.gross_amount}
+        Net Amount: {self.net_amount}
        """)
 
+    # This instance method calculates the gross and net win for each wheel/city played
     def calculate_gross_net_win(self, ticket):
-        TAX = (100 - 8) / 100
+        TAX = (100 - 8) / 100  # Tax = 8%
+        # Combinations can be achieved on a single wheel/city
         combinations = Win.combinations[len(self.extracted_numbers)][ticket.bet_type_id - 1]
+        # GROSS WINNING FOR A SINGLE WHEEL/CITY PLAYED
         amount = (Win.gross_winnings[len(ticket.numbers)][ticket.bet_type_id-1]) * combinations * ticket.money_put
-        if ticket.city.name == "Tutte":
+        if ticket.city_name.name == "Tutte":
+            # Subdivision gross winning between all wheels/cities played and instance attribute updating
             self.gross_amount = amount / (len(City.all_cities)-1)
         else:
+            # Instance attribute updating
             self.gross_amount = amount
+        # Instance attribute updating
         self.net_amount = self.gross_amount * TAX
+
 
 
 
